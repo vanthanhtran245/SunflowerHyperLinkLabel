748fcb0c09cfbf18b35ebaee667928dfdd692cf6
Tran Van Thanh <thanhtran.hce@gmail.com>
2018-10-18 13:36:58 +0700

    Initial Commit

---
+++ TVTHyperLabel/TVTHyperLabel.h
@@ -0,0 +1,19 @@
+//
+//  TVTHyperLabel.h
+//  TVTHyperLabel
+//
+//  Created by Thanh Tran Van on 10/18/18.
+//  Copyright © 2018 Thanh Tran Van. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TVTHyperLabel.
+FOUNDATION_EXPORT double TVTHyperLabelVersionNumber;
+
+//! Project version string for TVTHyperLabel.
+FOUNDATION_EXPORT const unsigned char TVTHyperLabelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TVTHyperLabel/PublicHeader.h>
+
+
